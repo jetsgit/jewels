8ee91097cc58f435c45e4b816c98e0578c4f1ff7
Jerrold Thompson <jet@whidbey.com>
2016-06-30 12:23:40 -0700

    rails g model Jewel

---
+++ spec/factories/jewels.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :jewel do
+    name "MyString"
+    description "MyText"
+    shine 1
+    price "9.99"
+    rarity 1
+    color "MyString"
+    faces 1
+  end
+end
