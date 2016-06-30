02bbe6dddfe408cf02760b407df149ee29932471
Jerrold Thompson <jet@whidbey.com>
2016-06-30 12:19:09 -0700

    setup api controller, routes, constraints

---
+++ app/controllers/api/jewels_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class JewelsController < ApplicationController
+      before_action :set_jewel, only: [:show, :edit,:update, :destroy]
+      respond_to :json
+      def index
+        respond_with  Jewel.all, content_type: 'application/flatlander; v=1'
+      end
+
+      private
+
+      def set_jewel
+        @jewel = Jewel.find(params[:id])
+      end
+
+      def jewel_params 
+        params.require(:jewel).permit(:name, :description, :shine, 
+                                   :price, :rarity, :color, :faces,
+                                   :images, :reviews)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+require 'api_constraints'
 Jewels::Application.routes.draw do
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
+      resources :jewels, only: [:index, :create, :update, :edit, :show]
+    end
+  end
   root 'home#index'
 end
