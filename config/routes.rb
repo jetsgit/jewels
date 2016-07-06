7fc5a45953ca4e3916b9a1ba6fbc2a82c299d273
Jerrold Thompson <jet@whidbey.com>
2016-07-06 11:42:22 -0700

    create nested routes for jewels/reviews and jewels/images

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ require 'api_constraints'
 Jewels::Application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
-      resources :jewels, only: [:index, :create, :update, :edit, :show]
+      resources :jewels, only: [:index, :create, :update, :edit, :show] do
+        resources :reviews, only: [:index, :create, :update, :show]
+        resources :images, only: [:index, :create, :update, :show]
+      end
     end
   end
   root 'home#angular'
