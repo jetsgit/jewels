35f7fe516cf3a357ebdff016657e847c1a865990
Jerrold Thompson <jet@whidbey.com>
2016-08-05 11:43:10 -0700

    change to app/jewels, add show method to jewels controller

--- app/controllers/api/v1/jewels_controller.rb
+++ app/controllers/api/v1/jewels_controller.rb
@@ -4,7 +4,11 @@ module Api
       before_action :set_jewel, only: [:show, :edit,:update, :destroy]
       respond_to :json
       def index
-        respond_with  Jewel.all.to_json(include: [:images, :reviews]), content_type: 'application/flatlander; v=1'
+        respond_with  Jewel.all.to_json(include: [:images, :reviews]), content_type: 'application/jewels; v=1'
+      end
+
+      def show
+        respond_with @jewel.to_json(include: [:images, :reviews]), content_type: 'application/jewels; v=1'
       end
 
       private
