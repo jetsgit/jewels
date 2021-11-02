7d62f2f37c3bc0056337fa86ce091ef5902742e1
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-03 07:39:17 +0800

    Delete spec directory

--- spec/factories/images.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :image do
-    name "MyString"
-    jewel nil
-  end
-end
--- spec/factories/reviews.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :review do
-    stars 1
-    author "MyString"
-    body "MyText"
-    jewel nil
-  end
-end
